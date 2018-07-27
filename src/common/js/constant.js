424b228c8b80fece00ad1ac581c2a5bf180076e3
lixin <lixin@jkweixin.com>
2018-07-27 11:59:22 +0800

    添加异业联盟活动，黄边

---
+++ src/common/js/constant.js
@@ -0,0 +1,5 @@
+export const IPC_ACTIVE_LIST = {
+  HUANG_BIAN: {
+    id: 36
+  }
+}
--- src/pages/activity/activity.vue
+++ src/pages/activity/activity.vue
@@ -1,18 +1,25 @@
 <template>
-  <article class="activity">
+  <article class="activity" v-if="a">
     <!--<section class="content">-->
     <!--<img class="activity-img" src="./actviity.jpg" alt="">-->
     <!--<div class="btn-a1" @touchstart="btnActivity" @touchend="btnActivityEnd"></div>-->
     <!--</section>-->
-    <section class="content">
+    <section class="content" v-if="a*1 === ipcActiveList.HUANG_BIAN.id ">
+      <img class="activity-img" src="./activity-hb.jpg" alt="">
+      <!--<div :class="['btn',hit?'btn-action':'']" @touchstart.prevent="btnActivity" @touchend.prevent="btnActivityEnd"></div>-->
+      <div class="btn-hb" @touchstart.prevent="btnActivity" @touchend.prevent="btnActivityEnd"></div>
+    </section>
+    <section class="content" v-else>
       <img class="activity-img" src="./actviity2.jpg" alt="">
       <div :class="['btn',hit?'btn-action':'']" @touchstart.prevent="btnActivity" @touchend.prevent="btnActivityEnd"></div>
     </section>
   </article>
 </template>
 <script type="text/ecmascript-6">
+  import {IPC_ACTIVE_LIST} from 'common/js/constant'
+
   export default {
-    data () {
+    data() {
       return {
         hit: false,
         st: false,
@@ -23,25 +30,30 @@
         type: ''
       }
     },
-    created () {
+    created() {
       this.m = this.$route.query.m || '' // 商家ID
       this.a = this.$route.query.a || ''// 活动ID
       this.e = this.$route.query.e || ''// 员工ID
       this.type = this.$route.query.type || ''// 活动类型
     },
     methods: {
-      btnActivity () {
+      btnActivity() {
         if (this.type === 'y') {
           this.hit = true
         }
       },
-      btnActivityEnd () {
+      btnActivityEnd() {
         if (this.type === 'y') {
           this.hit = false
           /* eslint-disable */
           let url = `/pages/activity-detail/activity-detail?m=${this.m}&a=${this.a}&e=${this.e}`
           wx.miniProgram.navigateTo({url})
         }
+      }
+    },
+    computed: {
+      ipcActiveList() {
+        return IPC_ACTIVE_LIST
       }
     }
   }
@@ -78,6 +90,16 @@
         left: 0px
         right: 0px
         height: 13vh
+        margin: auto
+        width: 90%
+        z-index: 9
+        touch-action: none
+      .btn-hb
+        position :absolute
+        bottom: 19vw
+        left: 0px
+        right: 0px
+        height: 15vw
         margin: auto
         width: 90%
         z-index: 9

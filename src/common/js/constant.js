7e7978f75eae956ee0c56a7afbbc271acce0bb5b
lixin <lixin@jkweixin.com>
2018-08-15 11:02:48 +0800

    异业联盟-添加金溪站-优化页面

--- src/common/js/constant.js
+++ src/common/js/constant.js
@@ -2,26 +2,26 @@ export const IPC_ACTIVE_LIST = [
   {
     name: '默认',
     id: -1,
-    imgUrl: '/static/yylm/activity_current.png'
+    imgUrl: '/yylm/activity_current.png'
   },
   {
     name: '黄边',
     id: 36,
-    imgUrl: '/static/yylm/activity-hb.png'
+    imgUrl: '/yylm/activity-hb.png'
   },
   {
     name: '汇桥',
     id: 37,
-    imgUrl: '/static/yylm/activity-hq-go.png'
+    imgUrl: '/yylm/activity-hq-go.png'
   },
   {
     name: '人和',
     id: 42,
-    imgUrl: '/static/yylm/activity-rh-go.png'
+    imgUrl: '/yylm/activity-rh-go.png'
   },
   {
     name: '京溪',
     id: 47,
-    imgUrl: '/static/yylm/activity-jx-go.png'
+    imgUrl: '/yylm/activity-jx-go.png'
   }
 ]

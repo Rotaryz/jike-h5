e7d3bf3b86b735d3bba34f0e4dd93766bb923a29
jtr <jtr354@foxmail.com>
2018-11-09 17:19:27 +0800

    异业联盟-东平站

--- src/common/js/constant.js
+++ src/common/js/constant.js
@@ -2,41 +2,41 @@ export const IPC_ACTIVE_LIST = [
   {
     name: '默认',
     id: -1,
-    imgUrl: '/yylm/activity_current.png'
+    imgUrl: require('@/pages/activity/yylm/activity_current.png')
   },
   {
     name: '黄边',
     id: 36,
-    imgUrl: '/yylm/activity-hb.png'
+    imgUrl: require('@/pages/activity/yylm/activity-hb.png')
   },
   {
     name: '汇桥',
     id: 37,
-    imgUrl: '/yylm/activity-hq-go.png'
+    imgUrl: require('@/pages/activity/yylm/activity-hq-go.png')
   },
   {
     name: '人和',
     id: 42,
-    imgUrl: '/yylm/activity-rh-go.png'
+    imgUrl: require('@/pages/activity/yylm/activity-rh-go.png')
   },
   {
     name: '京溪',
     id: 47,
-    imgUrl: '/yylm/activity-jx-go.png'
+    imgUrl: require('@/pages/activity/yylm/activity-jx-go.png')
   },
   {
     name: '永泰',
     id: 49,
-    imgUrl: '/yylm/activty_yt_go.png'
+    imgUrl: require('@/pages/activity/yylm/activty_yt_go.png')
   },
   {
     name: '新市',
     id: 52,
-    imgUrl: '/yylm/acticity_xs_go.png'
+    imgUrl: require('@/pages/activity/yylm/acticity_xs_go.png')
   },
   {
     name: '东平',
     id: 58,
-    imgUrl: '/yylm/active_dp_go.png'
+    imgUrl: require('@/pages/activity/yylm/active_dp_go.png')
   }
 ]
